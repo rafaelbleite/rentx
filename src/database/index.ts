3a2eef679ad3c759bf9c8d00bb8a061ce40366c8
Rafael Leite <rafaelleite.dev@gmail.com>
2021-03-27 11:38:52 -0300

    refactor: CreateSpecification

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,13 +1,17 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
 
-interface IOptions {
-  host: string;
-}
+createConnection();
 
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  newOptions.host = 'database_ignite'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  createConnection({
-    ...options,
-  });
-});
+// import { createConnection, getConnectionOptions } from 'typeorm';
+
+// interface IOptions {
+//   host: string;
+// }
+
+// getConnectionOptions().then(options => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = 'database_ignite'; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   createConnection({
+//     ...options,
+//   });
+// });
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,19 +1,25 @@
-import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+import { inject, injectable } from 'tsyringe';
+
+import { ISpecificationsRepository } from '../../repositories/ISpecificationsRepository';
 
 interface IRequest {
   name: string;
   description: string;
 }
 
+@injectable()
 class CreateSpecificationUseCase {
-  constructor(private specificationsRepository: ISpecificationsRepository) {}
+  constructor(
+    @inject('SpecificationsRepository')
+    private specificationsRepository: ISpecificationsRepository,
+  ) {}
 
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists = this.specificationsRepository.findByName(
-      name
+      name,
     );
     if (specificationAlreadyExists) {
-      throw new Error("Specification already exists!");
+      throw new Error('Specification already exists!');
     }
     this.specificationsRepository.create({ name, description });
   }
