f5e15a9b8cb796671802a996b2b1be5f0fa6ed44
Rafael Leite <rafaelleite.dev@gmail.com>
2021-03-27 12:20:23 -0300

    refactor: List Categories

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,13 +1,15 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
-  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
-  handle(request: Request, response: Response): Response {
-    const categories = this.listCategoriesUseCase.execute();
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
 
-    return response.status(200).json(categories);
+    const all = await listCategoriesUseCase.execute();
+
+    return response.status(200).json(all);
   }
 }
 
