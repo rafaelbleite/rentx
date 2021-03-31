713d9ba97fc9ccd1833489c201adb86c749b2fe5
Rafael Leite <rafaelleite.dev@gmail.com>
2021-03-30 23:53:21 -0300

    feat: add AppError

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { inject, injectable } from 'tsyringe';
 
+import { AppError } from '../../../../errors/AppError';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error('Email or Password incorrect!');
+      throw new AppError('Email or Password incorrect!');
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new Error('Email or Password incorrect!');
+      throw new AppError('Email or Password incorrect!');
     }
 
     const token = sign({}, '85c5a38f919e384738016911c241807d', {
@@ -42,13 +43,15 @@ class AuthenticateUserUseCase {
       expiresIn: '1d',
     });
 
-    return {
+    const tokenReturn: IResponse = {
+      token,
       user: {
         name: user.name,
         email: user.email,
       },
-      token,
     };
+
+    return tokenReturn;
   }
 }
 
