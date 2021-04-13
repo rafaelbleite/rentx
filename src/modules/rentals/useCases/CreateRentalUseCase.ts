b0247e7aaf04fbc2a06fb37202e99ca713f9eff3
Rafael Leite <rafaelleite.dev@gmail.com>
2021-04-12 23:49:24 -0300

    fix: availability of car after rental

--- src/modules/rentals/useCases/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -1,3 +1,4 @@
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { inject, injectable } from 'tsyringe';
 
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
@@ -19,6 +20,8 @@ class CreateRentalUseCase {
     private rentalsRepository: IRentalsRepository,
     @inject('DayjsDateProvider')
     private dateProvider: IDateProvider,
+    @inject('CarsRepository')
+    private carsRepository: ICarsRepository,
   ) {}
 
   async execute({
@@ -60,6 +63,8 @@ class CreateRentalUseCase {
       car_id,
       expected_return_date,
     });
+
+    await this.carsRepository.updateAvailable(car_id, false);
 
     return rental;
   }
