22e65c987ff2c0da6ea070af089276e263e2d22f
Rafael Leite <rafaelleite.dev@gmail.com>
2021-04-14 21:51:55 -0300

    refactor: ensure authentitcated with refresh token

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,6 @@
+import auth from '@config/auth';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
@@ -15,6 +17,8 @@ export async function ensureAuthenticated(
 ) {
   const authHeader = request.headers.authorization;
 
+  const userTokensRepository = new UsersTokensRepository();
+
   if (!authHeader) {
     throw new AppError('Token missing', 401);
   }
@@ -24,11 +28,13 @@ export async function ensureAuthenticated(
   try {
     const { sub: user_id } = verify(
       token,
-      '85c5a38f919e384738016911c241807d',
+      auth.secret_refresh_token,
     ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-    const user = await usersRepository.findById(user_id);
+    const user = await userTokensRepository.findByUserIdAndRefresh(
+      user_id,
+      token,
+    );
 
     if (!user) {
       throw new AppError('User not exists!', 401);
